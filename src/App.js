06df588b787f463dd8c03e83be8551781ca0525f
Jonathan Poblet <jonathanpoblet228@gmail.com>
2022-09-09 11:53:43 -0300

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ContactListComponent from './components/container/contact_list';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ContactListComponent></ContactListComponent>
       </header>
     </div>
